b8074576b1528ccbe949e772f1cc646649f32e03
Caroline Poveda <caroline.poveda.fl@gmail.com>
2026-05-11 20:12:07 +0200

    Terminé Phase 2 - Back jusque 2.6 (plant.router, plant.controller, plant.service)

--- backend/controllers/plant.controller.js
+++ backend/controllers/plant.controller.js
@@ -1,12 +1,26 @@
 const {Request, Response} = require('express');
 
+const plantService = require ('../services/plant.service');
+
 const plantController = {
     /**
      * @param {Request} req 
      * @param {Response} res
      */
     getAll : async(req, res) => {
-        res.send(501)
+        
+        const query = req.query;
+        console.log(query);
+
+        try {
+            const plants = await plantService.find(query);
+            res.status(200).json(plants);
+        } catch(err) {
+            console.log(err);
+            // Le controller reçoit l'erreur envoyée par le service => renvoie une réponse HTTP au client (le front) :
+            res.status(500).json( {statusCode : 500,
+                message : "Erreur avec la DB 😬"});
+        }
     },
 
     /**
@@ -14,9 +28,27 @@ const plantController = {
      * @param {Response} res
      */
     getById : async(req, res) => {
-        res.send(501)
-    }
+        
+        const id = req.params.id;
 
+        try {
+            const plant = await plantService.findById(id);
+
+            // Si plant est undefined ou null = si l'id recherché n'existe pas :
+            if (!plant) {
+                res.status(404).json( { statusCode : 404, message : "Plante introuvable"})
+            } 
+            // Si l'id existe, on renvoie la plante :
+            else {
+                res.status(200).json(plant);
+            }
+
+        } catch(err) {
+            console.log(err);
+            res.status(500).json( {statusCode : 500,
+                message : "Erreur avec la DB 😬"});
+        }
+    }
 }
 
 module.exports = plantController;
---
+++ backend/services/plant.service.js
@@ -0,0 +1,106 @@
+const Plant = require('../models/plant.model');
+
+const plantService = {
+
+    find : async(query) => {
+
+        try {
+            const filtrableFields = ['categories', 'sol', 'lumiere', 'humidite', 'floraison', 'plantation', 'cycle'];
+            
+            let filter = {};
+            
+            // Pour chaque champ de filtrable fields :
+            for (const field of filtrableFields) {
+
+                if(query[field] !== undefined) { // Si la propriété correspondant à field dans la query n'est PAS undefined = Si le champs a été coché :
+                    // Si Express voit plusieurs paramètres avec le même nom (ex : categories=aromatique&categories=mellifere), il les met automatiquement dans un tableau ; s'il ne voit qu'un seul param avec ce nom, il en fait une string ("aromatique")
+                    // Si query[field] est un tableau = Si il y a plusieurs cases cochées dans un des champs (ex : plusieurs catégories) :
+                    if (Array.isArray(query[field])) {
+                        // Ajouter à l'objet filter la propriété contenue dans field, si cette propriété correspond au contenu de field dans l'objet query :
+                        filter[field] = {$all : query[field]}
+                            // revient à faire filter.categories = { $in: query.categories }
+                            // [field] est entre crochets car la variale field est ici utilisée comme une clé dynamique : si on l'écrit sans, il est interprété comme un objet litéral 'field'.
+                    }
+                    // Si par contre query[field] n'est pas un tableau mais une string (= une seule case cochée pour ce champ), alors il faut le transformer manuellement en tableau, car $all ne peut chercher que dans un tableau => query[field] devient [query[field]] :
+                    else {
+                        filter[field] = {$all : [query[field]]}
+                    }
+                //  else {
+                    // Pas de else, ici on n'écrit rien : si on ajoute un filter = {} ça va réinitialiser filter comme un objet vide, or on veut qu'il conserve les autres propriétés => On n'écrit pas de else !
+                // }
+                }
+                    
+                
+            };       
+            // let categoryFilter = {categories : {$all : categories}};
+
+            if (query['toxicite.niveau'] !== undefined) { 
+                filter['toxicite.niveau'] = query['toxicite.niveau']
+                    // Ici, pas de $all, car on ne cherche pas une valeur parmi d'autres dans un tableau (categories, sol...) mais bien une correspondance directe : 
+                        // { $all: query[field] } = "cherche parmi dans la query la valeur qui correspond à ce field".
+                        // query['toxicite.niveau'] = "cherche exactement cette valeur dans la query".
+            };
+
+            if (query.search !== undefined) {
+                filter.$or = [
+                    {nom_commun : { $regex: query.search, $options: "i" }},
+                    {nom_latin : { $regex: query.search, $options: "i" }},
+                ] 
+            };
+            
+            
+            console.log(filter);
+
+            const plants = await Plant.find(filter)
+                                      .populate( {
+                                                path : 'associations',
+                                                select : {slug : 1,
+                                                nom_commun : 1,
+                                                nom_latin : 1}
+                                             })
+                                    //   .and(categoryFilter);
+            return plants;
+
+        } catch(err) {
+            console.log(err);
+
+            // Le service parle à la DB => Si ça plante, le service renvoie l'erreur au controller, qui l'attrapera à son tour dans osn catch :
+            throw new Error(err);
+        }     
+    },
+
+    findById : async(id) => {
+        try {
+            const searchedPlant = await Plant.findById(id)
+                                             .populate( {
+                                                path : 'associations',
+                                                select : {slug : 1,
+                                                nom_commun : 1,
+                                                nom_latin : 1}
+                                             })
+            return searchedPlant;
+        }
+        catch(err){
+            console.log(err);
+            throw new Error(err);
+        }
+    },
+
+    // findAssociationsById : async(id) => {
+    //     try {
+    //         const searchedPlant = await Plant.findById(id);
+
+
+
+    //         return searchedPlant;
+    //     }
+    //     catch(err){
+    //         console.log(err);
+    //         throw new Error(err);
+    //     }
+
+    // }
+
+}
+
+module.exports = plantService;
